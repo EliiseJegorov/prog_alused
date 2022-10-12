fe37586aded6ebe299298343059e8db1bfc82523
Eliise Jegorov <eliise.jegorov@voco.ee>
2022-10-12 19:22:03 +0300

    Ülesanne 1.3

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let aasta = 2022;
-let president = "Alar Karis";
-let lauseKeskosa = ". aastal valiti Eesti presidendiks ";
-let lause = aasta + lauseKeskosa + president
+let astmeAlus = Number(prompt("Sisestage astme alus"));
+let astendaja = Number(prompt("Sisestage astendaja"));
 
-console.log(lause);
+let vastus = Math.pow(astmeAlus,astendaja);
+console.log(vastus)
