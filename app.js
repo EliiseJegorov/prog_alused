d267130ef6415474073706e4c1748b81b27777fe
Eliise Jegorov <eliise.jegorov@voco.ee>
2022-10-12 19:12:14 +0300

    Ülesanne 1.2

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Tere, Maailm!")
+let aasta = 2022;
+let president = "Alar Karis";
+let lauseKeskosa = ". aastal valiti Eesti presidendiks ";
+let lause = aasta + lauseKeskosa + president
+
+console.log(lause);
