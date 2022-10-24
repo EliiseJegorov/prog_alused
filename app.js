0335be9ea44e754dffde71336c5db2dc189dde94
EliiseJegorov <eliise.jegorov@voco.ee>
2022-10-24 19:19:01 +0300

    Ülesanne 1.5

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-let Nimi = prompt("Sisesta oma nimi:");
-let lubatudKiirus = Number(prompt("Sisestage lubatud kiirus"));
-let tegelikKiirus = Number(prompt("Sisestage tegelik kiirus"));
+let ainepunktid = Number(prompt("Sisestage ainepunktide arv"));
+let nadalad = Number(prompt("Sisestage nädalate arv"));
 
-let trahv = (tegelikKiirus - lubatudKiirus) *3;
-let tegelikTrahv = Math.min(trahv, 190);
-
-console.log(Nimi + ", kiiruse yletamise eest on Teie trahv " + tegelikTrahv);
+let tunnid = ainepunktid * 26;
+let nadalasToimetan = tunnid / nadalad;
+console.log("Ühe nädala eeldatav ajakulu:" + Math.round(nadalasToimetan) + " tundi.");
